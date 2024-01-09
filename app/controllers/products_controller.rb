eb3225dc2771501470088b3fc015e188f630a854
Atakan <atakangobelek@satta.biz>
2024-01-09 17:37:03 +0300

    added edit update method for category and product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,40 @@
 class ProductsController < ApplicationController
 
-    def index
-        @products = Product.list(current_user.company_id)
+
+  def index
+    @products = Product.list(current_user.company_id)
+  end
+
+  def new
+    @product = Product.new
+    @categories = Category.list(current_user.company_id)
+  end
+
+  def create
+    @company = Company.find(params[:company_id])
+
+    @product = @company.products.build(product_params)
+
+    @product.save
+
+  end
+
+  def edit
+    @company = Company.find(params[:company_id])
+    @product = @company.products.find(params[:id])
+    puts "----#{@product.inspect}"
+    @categories = Category.list(current_user.company_id)
+  end
+
+  def update
+    @company = Company.find(params[:company_id])
+    @product = @company.products.find(params[:id])
+    if @product.update(product_params)
+
+    else
+      Hata
     end
-
-    def new
-        @product = Product.new
-        @categories = Category.list(current_user.company_id)
-    end
-
-    def create
-        @company = Company.find(params[:company_id])
-
-        @product = @company.products.build(product_params)
-
-        @product.save
-
-    end
+  end
 
 
 
