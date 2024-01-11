37f739f1c5d584e0edd525855a29660cdb6e584c
Atakan <atakangobelek@satta.biz>
2024-01-11 12:53:12 +0300

    added order-products model and integrated order model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ApplicationRecord
-    belongs_to :company
+  belongs_to :company
+  has_many :order_products
+  has_many :orders, through: :order_products
 
-    validates :name, presence: true
-    validates :price, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
     
 
 
-    scope :list, ->(company_id) { where(company_id: company_id )}
+  scope :list, ->(company_id) { where(company_id: company_id )}
 
 end
