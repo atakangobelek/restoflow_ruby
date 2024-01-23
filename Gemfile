3a8071e6682d089a39c46ec7f96ec47e460fe830
Atakan <atakangobelek@satta.biz>
2024-01-23 11:38:00 +0300

    added data grid

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "rolify", "~> 6.0"
 
 gem 'aasm'
 
+gem 'datagrid'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_183520) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_12_105442) do
   create_table "accounts", force: :cascade do |t|
     t.integer "order_id", null: false
     t.string "state"
@@ -53,6 +53,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_183520) do
     t.datetime "updated_at", null: false
     t.string "note"
     t.index ["company_id"], name: "index_orders_on_company_id"
+  end
+
+  create_table "orders_grids", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
